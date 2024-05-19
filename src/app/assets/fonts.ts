5e6abbb5763ddf73b65e7bad86ae613528618757
Williams <widorenyin@gmail.com>
2024-05-19 22:34:17 +0100

    chore: setup font and project layout

---
+++ src/app/assets/fonts.ts
@@ -0,0 +1,41 @@
+import localFont from "next/font/local";
+
+/**
+ * Creates local font instance
+ */
+export const Switzer = localFont({
+  src: [
+    {
+      path: "./fonts/Switzer-Black.woff2",
+      weight: "900",
+      style: "black",
+    },
+    {
+      path: "./fonts/Switzer-Bold.woff2",
+      weight: "800",
+      style: "bold",
+    },
+    {
+      path: "./fonts/Switzer-Medium.woff2",
+      weight: "700",
+      style: "medium",
+    },
+    {
+      path: "./fonts/Switzer-Regular.woff2",
+      weight: "500",
+      style: "regular",
+    },
+    {
+      path: "./fonts/Switzer-Light.woff2",
+      weight: "300",
+      style: "light",
+    },
+    {
+      path: "./fonts/Switzer-Thin.woff2",
+      weight: "100",
+      style: "light",
+    },
+  ],
+  variable: "--font-switzer",
+  display: "swap",
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const class_merge = (...classes: (string | boolean)[]): string =>
+  classes.filter(Boolean).join(" ");
